a9ddbe00f650ac427ec85ee22441d5f8c1c1f0d4
Rick <136603955+HeniqueSantos@users.noreply.github.com>
2025-04-06 19:57:24 -0300

    Add files via upload

---
+++ RevisaoLista.py
@@ -0,0 +1,16 @@
+#Revisao de lista
+
+# Crie uma lista vazia
+frutas = []
+
+# Adicione 3 frutas com append
+# Remova uma fruta com remove
+# Tire a última com pop
+# Mostre a lista no final
+
+frutas.append('Mexerica')
+frutas.append('Banana')
+frutas.append('Abobora')
+frutas.remove('Banana')
+frutas.pop()
+print(frutas)
